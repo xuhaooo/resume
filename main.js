d6b7b85174449f1d1fa54094caaef159c95cdaca
xuahaooo <bingoxuhao@gmail.com>
2020-05-21 15:05:09 +0800

    添加 loading

---
+++ main.js
@@ -0,0 +1,13 @@
+// sticky navbar
+// auto highlight navbar
+// auto scroll smoothly
+// menu
+// auto hide aside 
+// gabless slides
+// loading animation
+// animate when scroll
+
+
+setTimeout(function() {
+  siteWelcome.classList.remove('active')
+}, 1000)
